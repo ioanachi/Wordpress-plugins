7adab544257cf5e1eb7da323400337e2e1ed46c2
ioanachi <ioana_chichi@outlook.com>
2017-08-08 15:17:00 +0300

    Dolly

--- Tasks/Task 1.php
+++ Tasks/Task 1.php
@@ -53,7 +53,7 @@ Dolly'll never go away again";
 // This just echoes the chosen line, we'll position it later
 function hello_dolly() {
 	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>{}$chosen{}</p>";
+	echo '{'."<p id='dolly'>$chosen</p>".'}';
 }
 
 // Now we set that function up to execute when the admin_notices action is called
