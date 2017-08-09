51e70fe313df0448a13d118aa2c482016aed5651
ioanachi <ioana_chichi@outlook.com>
2017-08-09 21:14:43 +0300

    ......

--- Ioana plugin1/1plugin-0.0.1.php
+++ Ioana plugin1/1plugin-0.0.1.php
@@ -1,1 +1,10 @@
-<?php The first plugin ?>
+<?php
+/*=== Test Plugin ===
+Plugin Name: Plugin version 0.0.1
+Plugin URI:  https://no-url-yet.com
+Description: This is just a plugin test1
+Version:    0.01
+Author:      ioanachi
+Text Domain: wporg
+Domain Path: /languages
+*/
---
+++ Ioana plugin1/readme.txt
@@ -0,0 +1,10 @@
+/*=== Test Plugin ===
+Contributors: user, user, user
+Tags: tag, tag, tag
+Donate link: http://example.com/
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
