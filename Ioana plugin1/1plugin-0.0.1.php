ff1650f506ca1ace4f56807745607c2f2d0af922
ioanachi <ioana_chichi@outlook.com>
2017-08-10 07:04:45 +0300

    Change content

---
+++ Ioana plugin1/1plugin-0.0.1.php
@@ -0,0 +1,22 @@
+<?php
+/*=== Test Plugin ===
+Plugin Name: Plugin version 0.0.1
+Plugin URI:  https://no-url-yet.com
+Description: This is just a plugin test1
+Version:    0.01
+Author:      ioanachi
+Text Domain: wporg
+Domain Path: /languages
+*/
+add_action ("the_content", 'change' );
+
+function change ($content){
+$content = 'Nothing to see here';
+return $content;
+
+}
+
+
+
+
+?>
